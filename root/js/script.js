7a66f9d5d2c2b620696fdcaa87e9d4c0b30b09be
Leon F <aj199264@yahoo.com>
2020-09-13 16:37:44 -0400

    Commit 8

--- root/js/script.js
+++ root/js/script.js
@@ -1,19 +1,50 @@
-// gets the button through the DOM and stores it into a container to access it
-var scrollBtn = getElementById('scroll-to-top');
+// function for smooth scrolling
+function myFunction() {
 
-// window.onscroll = function() {
-// 	scroll()
-// };
-
-function scroll() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		scrollBtn.style.display = 'block';
-	} else {
-		scrollBtn.style.display = 'none';
+	const navLinks = document.querySelectorAll('nav a');
+	
+	for (let n in navLinks) {
+		if (navLinks.hasOwnProperty(n)) {
+			navLinks[n].addEventListener('click', function(e) {
+				e.preventDefault();
+				document.querySelector(navLinks[n].hash)
+				.scrollIntoView({
+					behavior: 'smooth'
+				});
+			});
+		}
 	}
+
 };
 
-function top() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-};
+// for scrollspy - change the active link as we scroll
+
+var home = document.getElementById('home');
+var aboutUs = document.getElementById('about-us');
+var services = document.getElementById('services');
+var contact = document.getElementById('contact');
+
+window.addEventListener('scroll', function() {
+	
+	var windo = window.pageYOffset;
+	
+	if (aboutUs.offsetTop >= windo && services.offsetTop > windo) {
+		console.log('About');
+	}
+	
+	else if (services.offsetTop >= windo && contact.offsetTop > windo) {
+		console.log('Services');
+	}
+	
+	else if (contact.offsetTop >= windo) {
+		console.log('Contact');
+	}
+	
+	else {
+		console.log('Home');
+	}
+	
+});
+
+// calls the smooth scrolling fn
+myFunction();
