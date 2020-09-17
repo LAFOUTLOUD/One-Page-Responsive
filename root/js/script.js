28b42d329cd8b808e49f1d9f6532c121aad87a59
Leon F <aj199264@yahoo.com>
2020-09-17 01:16:16 -0400

    Commit 9

--- root/js/script.js
+++ root/js/script.js
@@ -1,4 +1,5 @@
 // function for smooth scrolling
+
 function myFunction() {
 
 	const navLinks = document.querySelectorAll('nav a');
@@ -14,37 +15,36 @@ function myFunction() {
 			});
 		}
 	}
-
 };
 
 // for scrollspy - change the active link as we scroll
 
-var home = document.getElementById('home');
-var aboutUs = document.getElementById('about-us');
-var services = document.getElementById('services');
-var contact = document.getElementById('contact');
+//var home = document.getElementById('home');
+//var aboutUs = document.getElementById('about-us');
+//var services = document.getElementById('services');
+//var contact = document.getElementById('contact');
+//
+//window.addEventListener('scroll', function() {
+//	
+//	var windo = window.pageYOffset;
+//	
+//	if (aboutUs.offsetTop >= windo && services.offsetTop > windo) {
+//		console.log('About');
+//	}
+//	
+//	else if (services.offsetTop >= windo && contact.offsetTop > windo) {
+//		console.log('Services');
+//	}
+//	
+//	else if (contact.offsetTop >= windo) {
+//		console.log('Contact');
+//	}
+//	
+//	else {
+//		console.log('Home');
+//	}
+//	
+//});
 
-window.addEventListener('scroll', function() {
-	
-	var windo = window.pageYOffset;
-	
-	if (aboutUs.offsetTop >= windo && services.offsetTop > windo) {
-		console.log('About');
-	}
-	
-	else if (services.offsetTop >= windo && contact.offsetTop > windo) {
-		console.log('Services');
-	}
-	
-	else if (contact.offsetTop >= windo) {
-		console.log('Contact');
-	}
-	
-	else {
-		console.log('Home');
-	}
-	
-});
-
-// calls the smooth scrolling fn
+// calls the fns when the webpage is loaded
 myFunction();
